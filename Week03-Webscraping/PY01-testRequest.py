8240b4291fd9b13c18826bff266e30e095e7f576
David Markham <davidmarkham76@gmail.com>
2020-10-08 13:20:50 +0100

    Updated Test Request.

--- Week03-Webscraping/PY01-testRequest.py
+++ Week03-Webscraping/PY01-testRequest.py
@@ -1,11 +1,11 @@
 # David Markham
 # 10/10/2019 
 
-import requests
-from bs4 import BeautifulSoup
+import requests # import library
+from bs4 import BeautifulSoup # Imports beautiful soup to make page easier to read.
 page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
 print (page) # The header
 print("-------------------") 
 print (page.content) # The content
 soup1 = BeautifulSoup(page.content, 'html.parser')
-print (soup1.prettify()) # Makes it more beautiful with the parser
+print (soup1.prettify()) # Makes the page more user friendly to read, adjusts layout.
