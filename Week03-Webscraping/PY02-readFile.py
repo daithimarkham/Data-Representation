f910e003add22e3a470f3d1067da3ea9d252bd5e
David Markham <davidmarkham76@gmail.com>
2020-10-08 13:26:58 +0100

    Updated Read From File Request.

--- Week03-Webscraping/PY02-readFile.py
+++ Week03-Webscraping/PY02-readFile.py
@@ -2,10 +2,11 @@
 # 14/10/2019
 # Read from previous weeks html - Carviewer 
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # makes page more attractive and user friendly.
 
-with open("../Week02-Java/Lab2CarViewer.html") as fp:
+# Parsers are programs that read the text and convert that into DOM trees.
+with open("../Week02-Java/Lab2CarViewer.html") as fp: # opens file from directory.
     soup = BeautifulSoup(fp,"html.parser") # fp = file pointer
 
-print (soup.prettify())
+print (soup.prettify()) # Prints file and makes page more attractive for user.
 
