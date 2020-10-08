e60bdec53379631c4b6abac077409489a304bc33
David Markham <davidmarkham76@gmail.com>
2020-10-08 13:39:21 +0100

    Updated read out tags from file.

--- Week03-Webscraping/PY03-readOutFile.py
+++ Week03-Webscraping/PY03-readOutFile.py
@@ -1,25 +1,26 @@
 # David Markham
-# 10/10/2019 
+# 08/10/2020
 # Web scraping software may access the World Wide Web directly using the Hypertext Transfer Protocol,
 # or through a web browser.
 
+# Here we look for the first “tr” element in our file and print it out.
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # Makes page look more attractive.
 
-with open("../Week02-Java/Lab2CarViewer.html") as fp:
+with open("../Week02-Java/Lab2CarViewer.html") as fp: # fp = file pointer
     soup = BeautifulSoup(fp, 'html.parser')
 
-# print (soup.tr) # first element that has the tr tag
+# print (soup.tr) # This prints first element that has the tr tag in the file.
 
 
 
-rows = soup.findAll("tr") # tr defines a row in html
+rows = soup.findAll("tr") # tr defines a row in html. This will print out all tr tags.
 for row in rows:
     #print("........") # ..... put a line between each tr tag
     #print(row) # gets all the tr tags in each row
     
     datalist = [] # search for the td tags(table data) and modifies the data so stored in a list
-    cols = row.findAll("td")
+    cols = row.findAll("td") # Setting variable to find all the td tags.
     for col in cols:
-        datalist.append(col.text)
-    print (datalist) 
+        datalist.append(col.text) # Append datalist
+    print (datalist) # Prints datalist 
