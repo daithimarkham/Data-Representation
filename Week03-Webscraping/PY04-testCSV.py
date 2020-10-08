cf570443e02d5e6162fde65f8f5d77b2ac2a752d
David Markham <davidmarkham76@gmail.com>
2020-10-08 14:12:38 +0100

    Updated new file we created using CSV.

--- Week03-Webscraping/PY04-testCSV.py
+++ Week03-Webscraping/PY04-testCSV.py
@@ -2,12 +2,13 @@
 # Webcraping - CSV File
 # Write to a CSV file using the CSV package.
 
-import csv
+import csv # Library to import csv files.
 
-employee_file = open('employee_file.csv', mode='w')
+employee_file = open('employee_file.csv', mode='w') # what file to open. mode=w = write. Creates file in this directory.
+# Now we make a variable called employee_writer. Delimiter we use, quoate char will look like, and minimal amount of quiting is necessary.
 employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) 
 
-employee_writer.writerow(['John Smith', 'Accounting', 'November'])
-employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+employee_writer.writerow(['John Smith', 'Accounting', 'November']) # Write row
+employee_writer.writerow(['Erica Meyers', 'IT', 'March']) # Write row.
 
-employee_file.close()
+employee_file.close() # Then close file when finished with it.
